a7b484add38e5bed79a48dee5235881407391572
Iskandarov Timur <timur.iskandarov96@gmail.com>
2022-05-06 15:43:30 +0600

    Contact in footer

--- app/controllers/controller_main.php
+++ app/controllers/controller_main.php
@@ -26,6 +26,16 @@
 
             $data['footer']	= Model_Main::get_footer(LANG);
 
+            array_unshift($data['footer']['contacts_list'], array(
+                'isActive' => 1,
+                'title' => 'Exclusive Duet',
+                'address' => $data['site_adress'],
+                'site' => 'ex-duet.kz',
+                'phones' => array(
+                    $data['site_phone'],
+                )
+            ));
+
             if (PROJECT_ID !== ''):
                 $data['building_steps'] = Model_Building_Steps::get_steps(LANG);
             endif;
